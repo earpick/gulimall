1d9d5a4df8b5d6973284cf01ca1849cea8817047
jiangxing <1030486238@qq.com>
2021-07-10 23:06:00 +0800

    项目初始结构搭建完成

---
+++ gulimall-coupont/src/main/java/com/study/gulimall/coupont/GulimallCoupontApplication.java
@@ -0,0 +1,13 @@
+package com.study.gulimall.coupont;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallCoupontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallCoupontApplication.class, args);
+    }
+
+}
