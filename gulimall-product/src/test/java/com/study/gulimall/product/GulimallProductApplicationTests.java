63e446a46f2429aa13fe1129a33d0e2485214510
jiangxing <1030486238@qq.com>
2021-11-25 18:39:56 +0800

    p63 配置第三方服务模块

--- gulimall-product/src/test/java/com/study/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/study/gulimall/product/GulimallProductApplicationTests.java
@@ -1,5 +1,7 @@
 package com.study.gulimall.product;
 
+
+import com.aliyun.oss.OSSClient;
 import com.study.gulimall.product.entity.BrandEntity;
 import com.study.gulimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
@@ -8,6 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -15,6 +21,22 @@ class GulimallProductApplicationTests {
 
     @Autowired
     BrandService brandService;
+
+    @Autowired
+    OSSClient ossClient;
+
+    @Test
+    void testUpdateFile() throws FileNotFoundException {
+
+        // 填写本地文件的完整路径。如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
+        InputStream inputStream = new FileInputStream("C:\\Users\\king\\Pictures\\Saved Pictures\\2.jpg");
+
+        // 依次填写Bucket名称（例如examplebucket）和Object完整路径（例如exampledir/exampleobject.txt）。Object完整路径中不能包含Bucket名称。
+        ossClient.putObject("mygulimall-jx", "2.jpg", inputStream);
+
+        // 关闭OSSClient。
+        ossClient.shutdown();
+    }
 
     @Test
     void contextLoads() {
